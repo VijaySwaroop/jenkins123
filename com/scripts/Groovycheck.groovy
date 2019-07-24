6cae48a8048180c49686035bb7a0d8dd3b37fe4b
ahmed <faseeh27@gmail.com>
2019-07-24 19:44:33 +0530

    file

---
+++ com/scripts/Groovycheck.groovy
@@ -0,0 +1,11 @@
+package com.scripts;
+
+class Groovycheck{
+
+static def getSonarQubeAuth() {
+        def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+                org.jenkinsci.plugins.plaincredentials.StringCredentials.class, jenkins.model.Jenkins.instance
+        )
+		}
+		}
+
