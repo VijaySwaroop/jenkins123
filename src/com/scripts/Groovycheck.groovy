e722d3d9b5306a0cffe41d12b0255f16b078fabf
rkar11 <rvar11@gmail.com>
2019-07-25 12:20:00 +0530

    new6

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -6,10 +6,8 @@ import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
 class Groovycheck{
 
 static def getSonarQub() {
-        def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-                org.jenkinsci.plugins.plaincredentials.StringCredentials.class, jenkins.model.Jenkins.instance
-        )
-	return creds
+        for (credential in CredentialsProvider.lookupCredentials(StandardUsernameCredentials.class)) {
+          return credential;
+      }
 		}
 		}
-
