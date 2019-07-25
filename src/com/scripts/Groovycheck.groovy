9504894c8f06722ceb512c823438c23563b2659f
rkar11 <rvar11@gmail.com>
2019-07-25 12:12:47 +0530

    new6

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -1,5 +1,8 @@
 package com.scripts;
 
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+
 class Groovycheck{
 
 static def getSonarQub() {
