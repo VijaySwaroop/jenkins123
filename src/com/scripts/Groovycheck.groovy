f5e7fc46deaa938956eee0c70013f65015b38f36
rkar11 <rvar11@gmail.com>
2019-07-26 22:54:21 +0530

    neww

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -5,9 +5,6 @@ import hudson.model.AbstractBuild;
 def n
 public abstract class Groovycheck extends AbstractBuild{
 
-def Groovycheck(def n){
-this.n=n
-}
 static def getSonarQub() {
 		def build = new Groovycheck(n)
 		return build.getWorkspace()
