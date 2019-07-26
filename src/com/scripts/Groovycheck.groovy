e82356862b9aad4b40eeba8473e0b85388946b28
rkar11 <rvar11@gmail.com>
2019-07-26 22:13:21 +0530

    neww

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -1,16 +1,13 @@
 package com.scripts;
 
 import hudson.model.AbstractBuild;
-import com.cloudbees.plugins.credentials.CredentialsProvider;
-import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+
 
 class Groovycheck{
 
 static def getSonarQub() {
 		def build = new AbstractBuild()
 		return build.getWorkspace()
-        //for (credential in CredentialsProvider.lookupCredentials(StandardUsernameCredentials.class)) {
-        //  return credential;
-      }
-		}
-		}
+        
+        }
+}
