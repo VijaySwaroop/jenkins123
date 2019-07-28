77807fe4ee8b475f259217043e257fc5a77c8873
rkar11 <rvar11@gmail.com>
2019-07-28 22:12:54 +0530

    git commit

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -4,12 +4,14 @@ package com.scripts;
 import hudson.model.ParametersAction;
 import hudson.model.StringParameterValue;
 import hudson.model.FreeStyleBuild;
+import java.*;
 
 class Groovycheck {
 
 static def getSonarQub() {
         FreeStyleBuild build = new FreeStyleBuild()
-		return build.getWorkspace()
+		System.out.println(build.getWorkspace())
+		//return build.getWorkspace()
 		//return manager.listener.logger.println
 		
         }
