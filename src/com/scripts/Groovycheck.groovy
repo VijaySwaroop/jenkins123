30a5cd2d125446a701059cf7d354aabac405c025
rkar11 <rvar11@gmail.com>
2019-07-28 22:07:33 +0530

    git commit

--- src/com/scripts/Groovycheck.groovy
+++ src/com/scripts/Groovycheck.groovy
@@ -1,12 +1,16 @@
 package com.scripts;
 
-import hudson.model.AbstractBuild;
+
 import hudson.model.ParametersAction;
 import hudson.model.StringParameterValue;
+import hudson.model.Build;
 
 class Groovycheck {
 
 static def getSonarQub() {
+        Build build = new Build()
+		build.getWorkspace()
 		return manager.listener.logger.println
+		
         }
 }
